f31dc8e368420d2c80f72922e41cfdf208965560
Alfonso Sarno <alfonsosarno1@icloud.com>
2025-08-06 15:22:54 +0200

    first commit

---
+++ src/redux/reducers/index.js
@@ -0,0 +1,10 @@
+// qua ce il robottino che legge le richieste e fa i cambiamenti
+
+const initialState = {
+  cart: {
+    content: [],
+  },
+};
+
+// lo stato attuale e quello futuro
+const mainReducer = (state = initialState, action) => {};
